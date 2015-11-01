56885c36a9aa36f8c2ef604d3aac21434f90597e
Adam Pahlevi <adam.pahlevi@gmail.com>
2015-11-01 19:05:13 +0700

    remove Pismo::Reader::Document, simplified to Pismo::Reader

--- lib/pismo/reader.rb
+++ lib/pismo/reader.rb
@@ -1,19 +1,18 @@
 module Pismo
   module Reader
-    class Document
+    
+    module_function
 
-      def self.create(raw_content, options = {})
-        type = options.delete(:reader)
-        case type
-        when :score
-          Pismo::Reader::Tree.new(raw_content, options)
-        when :cluster
-          Pismo::Reader::Cluster.new(raw_content, options)
-        else
-          Pismo::Reader::Tree.new(raw_content, options)
-        end
+    def create(raw_content, options = {})
+      type = options.delete(:reader)
+      case type
+      when :score
+        Pismo::Reader::Tree.new(raw_content, options)
+      when :cluster
+        Pismo::Reader::Cluster.new(raw_content, options)
+      else
+        Pismo::Reader::Tree.new(raw_content, options)
       end
-
     end
   end
 end
