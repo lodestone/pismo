e04696464db4c1c726c73c29fc5d5815ffa78cbf
Adam Pahlevi <adam.pahlevi@gmail.com>
2015-11-01 19:22:36 +0700

    add rspec spec_helper

---
+++ spec/spec_helper.rb
@@ -0,0 +1,15 @@
+require "shoulda"
+require "open-uri"
+require "yaml"
+require "pismo"
+
+HTML_DIRECTORY = File.expand_path("../spec/corpus", File.dirname(__FILE__)) 
+RSpec.configure do |config|
+  config.expect_with :rspec do |expectations|
+    expectations.include_chain_clauses_in_custom_matcher_descriptions = true
+  end
+
+  config.mock_with :rspec do |mocks|
+    mocks.verify_partial_doubles = true
+  end
+end
